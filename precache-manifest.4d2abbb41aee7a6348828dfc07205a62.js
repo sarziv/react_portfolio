a39dedd52aedba92ffe235bb31f9acc9ae7639af
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-07 04:25:45 +0000

    Deploy sarziv/react_portfolio to github.com/sarziv/react_portfolio.git:gh-pages

---
+++ precache-manifest.4d2abbb41aee7a6348828dfc07205a62.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8c24a4100a0704b8ae77866892ad14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652e62710c09677863c3",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "158b24850929a59d4301",
+    "url": "/static/css/main.c6770ea3.chunk.css"
+  },
+  {
+    "revision": "652e62710c09677863c3",
+    "url": "/static/js/2.d5b130bb.chunk.js"
+  },
+  {
+    "revision": "158b24850929a59d4301",
+    "url": "/static/js/main.d89c63ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a2c60ed4d95b52b29ebe49b74787d3f1",
+    "url": "/static/media/apipic.a2c60ed4.png"
+  },
+  {
+    "revision": "a420a5065537e7fdc692e40236598317",
+    "url": "/static/media/ascitabel.a420a506.png"
+  },
+  {
+    "revision": "d55df38289e03b90ec010d4e94f65698",
+    "url": "/static/media/developer-dribbble.d55df382.gif"
+  },
+  {
+    "revision": "48c66d96a5a74adfa567cb7e9ff3900d",
+    "url": "/static/media/findmap.48c66d96.png"
+  },
+  {
+    "revision": "846235e40ac5b1c46c29d5987b4b0be7",
+    "url": "/static/media/portgif.846235e4.gif"
+  },
+  {
+    "revision": "816a055b2a51e33061af5b8ff87f69fb",
+    "url": "/static/media/salarypic1.816a055b.png"
+  },
+  {
+    "revision": "1773d343a0176db6ff837bf7f5947b7a",
+    "url": "/static/media/salarypic2.1773d343.png"
+  }
+]);
